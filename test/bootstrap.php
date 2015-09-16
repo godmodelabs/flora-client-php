baecd07186398d11f301a39b4758811238dfadb1
Marco Baumgartl <marco.baumgartl@boerse-go.de>
2015-09-16 14:35:12 +0200

    Remove test-specific bootstrapping

--- test/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-// TODO: remove once php-auth-client is available as seperate composer package
-/** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__  . '/../vendor/autoload.php';
-$loader->addPsr4('Flora\\Auth\\', __DIR__ . '/../../php-auth-client/src/Flora/Auth/');
-$loader->addPsr4('League\\OAuth2\\Client\\', __DIR__ . '/../../php-auth-client/vendor/league/oauth2-client/src/');
